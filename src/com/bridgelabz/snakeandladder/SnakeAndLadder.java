0470a6e0b1b1ea584c0b34788ed5acf904e11a83
padmini v <padmini.v@perfios.com>
2021-08-23 15:29:47 +0530

    ADDED: Dice functionality

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,7 +4,9 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("*****Welcome to the game of Snakes and Ladders*****");
+		System.out.println("***WELCOME TO THE GAME OF SNAKES AND LADDERS***");
+		int valueOfDie=(int)(Math.random()*6) +1;
+		System.out.println("The value of die is "+ valueOfDie);
 
 	}
 
