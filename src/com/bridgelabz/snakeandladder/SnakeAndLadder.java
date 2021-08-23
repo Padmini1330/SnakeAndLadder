f2fa47b9c9d1394b8ed5cf38bb973ee518d2d1a9
padmini v <padmini.v@perfios.com>
2021-08-23 15:36:56 +0530

    ADDED: noplay ladder and snake

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,12 +1,27 @@
 package com.bridgelabz.snakeandladder;
 
-public class SnakeAndLadder {
+public class SnakeAndLadder 
+{
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		int position=0;
 		System.out.println("***WELCOME TO THE GAME OF SNAKES AND LADDERS***");
 		int valueOfDie=(int)(Math.random()*6) +1;
 		System.out.println("The value of die is "+ valueOfDie);
+		int option=(int)(Math.random()*3) +1;
+		switch(option)
+		{
+			case 1: System.out.println("positionition:"+position);
+			        break;
+			case 2: position+=valueOfDie;
+			        System.out.println("positionition:"+position);
+					break;
+			case 3: position-=valueOfDie;
+					System.out.println("positionition:"+position);
+					break;
+			default:break;
+		}
 
 	}
 
