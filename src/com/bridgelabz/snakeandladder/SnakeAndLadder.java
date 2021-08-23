eb3a0d4e240bb098ba434b0809c78427c01cbb3b
padmini v <padmini.v@perfios.com>
2021-08-23 15:22:10 +0530

    ADDED: Welcome message

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("*****Welcome to the game of Snakes and Ladders*****");
+
+	}
+
+}
