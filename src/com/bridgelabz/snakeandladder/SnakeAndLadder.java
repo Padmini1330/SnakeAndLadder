a9e40de25470f4a89f85960fc366579c1b819182
padmini v <padmini.v@perfios.com>
2021-08-23 15:40:42 +0530

    ADDED: winning position

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.snakeandladder;
 
+
 public class SnakeAndLadder 
 {
 
@@ -7,22 +8,39 @@ public class SnakeAndLadder
 	{
 		int position=0;
 		System.out.println("***WELCOME TO THE GAME OF SNAKES AND LADDERS***");
-		int valueOfDie=(int)(Math.random()*6) +1;
-		System.out.println("The value of die is "+ valueOfDie);
-		int option=(int)(Math.random()*3) +1;
-		switch(option)
+		
+		while(position!=100) 
 		{
-			case 1: System.out.println("positionition:"+position);
-			        break;
-			case 2: position+=valueOfDie;
-			        System.out.println("positionition:"+position);
-					break;
-			case 3: position-=valueOfDie;
-					System.out.println("positionition:"+position);
-					break;
-			default:break;
+			
+			int valueOfDie=(int)(Math.random()*6) +1;
+			System.out.println("The value of die is "+ valueOfDie);
+			int option=(int)(Math.random()*3) +1;
+			switch(option)
+			{
+				case 1: System.out.println("position:"+position);
+				        break;
+				case 2: position+=valueOfDie;
+						if(position>100)
+						{
+							position-=valueOfDie;
+						}
+				        System.out.println("position:"+position);
+						if(position==100)
+							break;
+						
+						break;
+				case 3: position-=valueOfDie;
+						if(position<0)
+						{
+							position=0;
+						}
+						System.out.println("position:"+position);
+						break;
+				default:break;
+			}
+
 		}
 
-	}
-
+    }
 }
+
